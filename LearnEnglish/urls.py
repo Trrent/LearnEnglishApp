87d29371feb5672ec0e2b4eb3f58a1a8c88991d2
Dinislam <dinislamsayfullin@gmail.com>
2023-10-24 00:14:57 +0300

    update urls.py

--- LearnEnglish/urls.py
+++ LearnEnglish/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from LearnEnglish import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +28,7 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    if settings.MEDIA_ROOT:
+        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+        urlpatterns += staticfiles_urlpatterns()
