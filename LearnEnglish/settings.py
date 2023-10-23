1ad6489ea98a64b7508a5533f701244ff3f86b26
Dinislam <dinislamsayfullin@gmail.com>
2023-10-24 00:12:35 +0300

    update settings.py

--- LearnEnglish/settings.py
+++ LearnEnglish/settings.py
@@ -97,10 +97,6 @@ WSGI_APPLICATION = 'LearnEnglish.wsgi.application'
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-
 AUTH_USER_MODEL = 'api.User'
 
 # Database
@@ -158,7 +154,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'assets'))]
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
