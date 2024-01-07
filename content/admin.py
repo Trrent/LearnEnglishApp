c8f66cad8df5ccfc90c0a04bf0514ec72c5f4f59
Dinislam <dinislamsayfullin@gmail.com>
2024-01-07 21:29:40 +0300

    add new models, init admin panel

--- content/admin.py
+++ content/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from .models import Course, Season, Episode
+from .models import Serial, Season, Episode, Content, IText, IVideo, IQuiz
 
 
-@admin.register(Course)
-class CourseAdmin(admin.ModelAdmin):
+@admin.register(Serial)
+class SerialAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'slug']
     prepopulated_fields = {'slug': ('title',)}
 
@@ -21,7 +21,33 @@ class EpisodeInline(admin.StackedInline):
 @admin.register(Season)
 class SeasonAdmin(admin.ModelAdmin):
     list_display = ['slug', 'created']
-    list_filter = ['created', 'course']
+    list_filter = ['created', 'serial']
     search_fields = ['title', 'overview']
     prepopulated_fields = {'slug': ('title',)}
     inlines = [EpisodeInline]
+
+
+class ContentInline(admin.StackedInline):
+    model = Content
+
+
+@admin.register(IText)
+class ITextAdmin(admin.ModelAdmin):
+    list_display = ['id', 'rus']
+    inlines = [ContentInline]
+
+
+@admin.register(IVideo)
+class IVideoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+@admin.register(IQuiz)
+class IQuizAdmin(admin.ModelAdmin):
+    list_display = ['id']
+
+
+@admin.register(Content)
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ['episode', 'item']
+    # inlines = [ITextInline]
--- content/models.py
+++ content/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+
 import uuid
 
 
@@ -6,58 +9,7 @@ def get_video_path(instance, filename):
     return f"{instance.lesson.title}/Season {instance.lesson.season_num}/Episode {instance.lesson.episode_num}/{filename}"
 
 
-# class VideoPart(models.Model):
-#     uuid = models.UUIDField(default=uuid.uuid4, unique=True)
-#     name = models.CharField(max_length=180)
-#     src = models.FileField(upload_to=get_video_path, max_length=180)
-#     subtitles = models.CharField(max_length=180, blank=True)
-#     poster = models.FileField(upload_to=get_video_path, max_length=180, blank=True)
-#     quality_src = models.CharField(max_length=180)
-#
-#     def __str__(self):
-#         return self.task
-#
-#
-# class Question(models.Model):
-#     uuid = models.UUIDField(default=uuid.uuid4, unique=True)
-#     question = models.CharField(max_length=180, blank=False)
-#     right_answer = models.CharField(max_length=180, blank=False)
-#
-#     def __str__(self):
-#         return self.question
-#
-#
-# class Answer(models.Model):
-#     uuid = models.UUIDField(default=uuid.uuid4, unique=True)
-#     question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
-#     answer = models.CharField(max_length=180, blank=True)
-#
-#     def __str__(self):
-#         return f"Answer: {self.answer}"
-#
-#
-# class Lesson(models.Model):
-#     uuid = models.UUIDField(default=uuid.uuid4, unique=True)
-#     title = models.CharField(max_length=180, blank=False)
-#     description = models.CharField(max_length=500, blank=True)
-#     season_num = models.IntegerField(blank=False)
-#     episode_num = models.IntegerField(blank=False)
-#     video_src = models.ForeignKey(VideoPart, on_delete=models.CASCADE, related_name='lesson', blank=True)
-#
-#     def __str__(self):
-#         return f"{self.title} ({self.season_num} season, {self.episode_num} episode"
-#
-#
-# class QuizPart(models.Model):
-#     uuid = models.UUIDField(default=uuid.uuid4, unique=True)
-#     questions = models.CharField(blank=True)
-#     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, related_name='quiz_src')
-#
-#     def __str__(self):
-#         return self.questions
-
-
-class Course(models.Model):
+class Serial(models.Model):
     id = models.CharField(default=uuid.uuid4, primary_key=True)
     title = models.CharField(max_length=180, blank=False)
     slug = models.SlugField(max_length=200, unique=True)
@@ -72,7 +24,7 @@ class Course(models.Model):
 class Season(models.Model):
     id = models.CharField(default=uuid.uuid4, primary_key=True)
     title = models.CharField(max_length=200)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='seasons')
+    serial = models.ForeignKey(Serial, on_delete=models.CASCADE, related_name='seasons')
     slug = models.SlugField(max_length=200, unique=True)
     overview = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
@@ -81,7 +33,7 @@ class Season(models.Model):
         ordering = ('-created',)
 
     def __str__(self):
-        return f"{self.course.title} {self.title}"
+        return f"{self.serial.title} {self.title}"
 
 
 class Episode(models.Model):
@@ -90,4 +42,87 @@ class Episode(models.Model):
     title = models.CharField(max_length=200)
 
     def __str__(self):
-        return f"{self.season.course.title} ({self.season.title} {self.title})"
+        return f"{self.season.serial.title} ({self.season.title} {self.title})"
+
+
+class ItemBase(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    # content = GenericRelation('Content')
+
+    class Meta:
+        abstract = True
+
+
+class Content(models.Model):
+    episode = models.ForeignKey(Episode, on_delete=models.CASCADE, related_name='contents')
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, limit_choices_to={'model__in': ('itext',
+                                                                                                            'ivideo',
+                                                                                                            'iquiz')})
+    object_id = models.CharField()
+    item = GenericForeignKey('content_type', 'object_id')
+
+
+class IText(ItemBase):
+    rus = models.TextField()
+    eng = models.TextField()
+
+    # content = GenericRelation('Content')
+
+    def __str__(self):
+        return f"IText (rus: {self.rus} - eng: {self.eng})"
+
+
+class IVideo(ItemBase):
+    name = models.CharField(max_length=250)
+    subtitles = models.CharField(blank=True)
+    poster = models.FileField(upload_to='images')
+
+    def __str__(self):
+        return f"IVideo (name: {self.name})"
+
+
+class Video(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    quality = models.CharField(max_length=10)
+    src = models.URLField()
+    iVideo = models.ForeignKey(IVideo, on_delete=models.CASCADE, related_name='qualitySrc')
+
+
+class IQuiz(ItemBase):
+    QUIZ_TYPE = (
+        ('quiz', 'quiz'),
+        ('test', 'test'),
+    )
+
+    type = models.CharField(max_length=10, choices=QUIZ_TYPE, default='quiz')
+    score = models.IntegerField(blank=False, default=0)
+
+    def __str__(self):
+        return f"IQuiz ({self.type})"
+
+
+class Question(models.Model):
+    QUESTION_TYPE = (
+        ('Yes/No', 'Yes/No'),
+        ('multiple-choice', 'multiple-choice'),
+        ('select', 'select'),
+    )
+
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    quiz_id = models.ForeignKey(IQuiz, on_delete=models.CASCADE, related_name='questions')
+    type = models.CharField(max_length=20, choices=QUESTION_TYPE, default='quiz')
+    score = models.IntegerField(blank=False, default=0)
+    content = models.CharField(max_length=250)
+    active = models.BooleanField(default=True)
+
+
+class Answer(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    quiz_id = models.ForeignKey(IQuiz, on_delete=models.CASCADE, related_name='answers')
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+    content = models.CharField(max_length=250)
+    correct = models.BooleanField(default=False)
+    active = models.BooleanField(default=True)
