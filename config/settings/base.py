aac314a8f9db82bd512a0d59b8c5f477673ac66c
Dinislam <dinislamsayfullin@gmail.com>
2024-03-04 22:19:32 +0300

    change STATICFILES_DIRS

--- config/settings/base.py
+++ config/settings/base.py
@@ -180,8 +180,8 @@ USE_TZ = True
 
 # STATIC_URL = '/static/'
 #
-# STATICFILES_DIRS = [BASE_DIR / 'static', ]
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static', ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
@@ -189,9 +189,9 @@ MEDIA_URL = '/media/'
 # STATIC_ROOT = os.path.join(STATIC_DIR, 'static/')
 MEDIA_ROOT = os.path.join(STATIC_DIR, 'media/')
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
